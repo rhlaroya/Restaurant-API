39c1eeeddc12017db2145001e5d88280dd690456
rhlaroya <rhlaroya@gmail.com>
2021-03-23 11:10:40 -0500

    Initializing Project

---
+++ RestaurantAPI/src/test/java/com/cognixia/jump/restaurant/RestaurantApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognixia.jump.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
