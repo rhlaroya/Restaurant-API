25d4adb9d7eaab8db37ab9f9fd46332627440284
rhlaroya <rhlaroya@gmail.com>
2021-03-23 11:18:58 -0500

    Restaurant Model

---
+++ RestaurantAPI/src/main/java/com/cognixia/jump/restaurant/model/Restaurant.java
@@ -0,0 +1,79 @@
+package com.cognixia.jump.restaurant.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class Restaurant implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "restaurant_id")
+	private long id;
+	
+	private String name;
+	private String address;
+	private String description;
+	
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public Restaurant(long id, String name, String address, String description) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.address = address;
+		this.description = description;
+	}
+
+	public Restaurant() {
+		this(-1, "N/A", "N/A", "N/A");
+	}
+
+	@Override
+	public String toString() {
+		return "Restaurant [id=" + id + ", name=" + name + ", address=" + address + ", description=" + description
+				+ "]";
+	}
+	
+}
