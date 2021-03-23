c587f446eacdcd612fb646d210adb57394d203c2
rhlaroya <rhlaroya@gmail.com>
2021-03-23 11:28:03 -0500

    User Repository

---
+++ RestaurantAPI/src/main/java/com/cognixia/jump/restaurant/repository/UserRepository.java
@@ -0,0 +1,22 @@
+package com.cognixia.jump.restaurant.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cognixia.jump.restaurant.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	Optional<User> findById(Long id);
+	
+	List<User> findByFirstName(String firstName);
+	
+	List<User> findByLastName(String lastName);
+	
+	List<User> findByEmail(String email);
+	
+}
