896600297ce7bcd7fcde20b950c524e0f4a8c3b6
rhlaroya <rhlaroya@gmail.com>
2021-03-24 23:08:16 -0500

    Changed Implementation to check is a user present before returning user details.

--- RestaurantAPI/src/main/java/com/cognixia/jump/restaurant/service/MyUserDetailsService.java
+++ RestaurantAPI/src/main/java/com/cognixia/jump/restaurant/service/MyUserDetailsService.java
@@ -15,11 +15,15 @@ import com.cognixia.jump.restaurant.repository.UserRepository;
 public class MyUserDetailsService implements UserDetailsService {
 
 	@Autowired
-	UserRepository userRepository;
+	UserRepository repo;
 	
 	@Override
 	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
-		Optional<User> user = userRepository.findByUserName(userName);
-		return new MyUserDetails(userName);
+		Optional<User> user = repo.findByUserName(userName);
+		
+		if(user.isEmpty()) {
+			throw new UsernameNotFoundException("username: " + userName + " does not exist");
+		}
+		return new MyUserDetails(user.get());
 	}
 }
