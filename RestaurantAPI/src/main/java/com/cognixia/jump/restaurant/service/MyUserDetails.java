9849a6ab40136a03cf5876fa591c428338fba923
rhlaroya <rhlaroya@gmail.com>
2021-03-24 23:07:21 -0500

    Added a List for passing values of User to authorities

--- RestaurantAPI/src/main/java/com/cognixia/jump/restaurant/service/MyUserDetails.java
+++ RestaurantAPI/src/main/java/com/cognixia/jump/restaurant/service/MyUserDetails.java
@@ -2,11 +2,14 @@ package com.cognixia.jump.restaurant.service;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import com.cognixia.jump.restaurant.model.User;
 
 @EnableWebSecurity
 public class MyUserDetails implements UserDetails {
@@ -14,23 +17,32 @@ public class MyUserDetails implements UserDetails {
 	private static final long serialVersionUID = 1L;
 	
 	private String userName;
+	
+	private String password;
+	
+	private List<GrantedAuthority> authorities;
 
 	public MyUserDetails(String userName) {
 		this.userName = userName;
 	}
 	
 	public MyUserDetails() {
-		// TODO Auto-generated constructor stub
+	}
+	
+	public MyUserDetails(User user){
+		this.userName = user.getUserName();
+		this.password = user.getPassword();
+		this.authorities = Arrays.asList(new SimpleGrantedAuthority(user.getRole().toString()));
 	}
 
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return Arrays.asList(new SimpleGrantedAuthority("USER"));
+		return authorities;
 	}
 
 	@Override
 	public String getPassword() {
-		return "root";
+		return password;
 	}
 
 	@Override
