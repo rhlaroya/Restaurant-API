88f406cd76498aa4913b2473251e330566f6873e
rhlaroya <rhlaroya@gmail.com>
2021-03-25 15:36:55 -0500

    Added Cross Origin for React

--- RestaurantAPI/src/main/java/com/cognixia/jump/restaurant/controller/UserController.java
+++ RestaurantAPI/src/main/java/com/cognixia/jump/restaurant/controller/UserController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -18,9 +19,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.cognixia.jump.restaurant.model.User;
 import com.cognixia.jump.restaurant.repository.UserRepository;
 
-
-
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
 
 	@Autowired
