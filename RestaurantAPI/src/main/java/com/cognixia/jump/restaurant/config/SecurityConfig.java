77da4aae3373f4aef6867cb95466f95364eac2a8
rhlaroya <rhlaroya@gmail.com>
2021-03-24 23:05:57 -0500

    Created constructor to pull granted authorities from User

--- RestaurantAPI/src/main/java/com/cognixia/jump/restaurant/config/SecurityConfig.java
+++ RestaurantAPI/src/main/java/com/cognixia/jump/restaurant/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -31,5 +32,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 	
 	@Bean
-	public PasswordEncoder getPasswordEncoder() {return NoOpPasswordEncoder.getInstance(); }
+	public PasswordEncoder getPasswordEncoder() {return PasswordEncoderFactories.createDelegatingPasswordEncoder(); }
 }
