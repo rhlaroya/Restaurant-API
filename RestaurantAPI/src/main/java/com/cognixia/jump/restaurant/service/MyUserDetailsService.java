a61862654a97297a1a51ef73a5bcfe33272d036b
rhlaroya <rhlaroya@gmail.com>
2021-03-24 22:35:24 -0500

    Initializing MyUserDetails

---
+++ RestaurantAPI/src/main/java/com/cognixia/jump/restaurant/service/MyUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.cognixia.jump.restaurant.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.cognixia.jump.restaurant.model.User;
+import com.cognixia.jump.restaurant.repository.UserRepository;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	UserRepository userRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+		Optional<User> user = userRepository.findByUserName(userName);
+		return new MyUserDetails(userName);
+	}
+}
